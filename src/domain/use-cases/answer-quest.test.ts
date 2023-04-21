aa2c2b3b3fbd0983e3a96fb3133739fe3953e5c6
andreenakashima <andreenakashima@gmail.com>
2023-04-21 15:58:17 -0300

    First use case

---
+++ src/domain/use-cases/answer-quest.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+
+test("create a answer", () => {
+	const answerQuestion = new AnswerQuestionUseCase();
+	const answer = answerQuestion.execute({
+		instructorId: "1",
+		questionId: "1",
+		content: "Nova resposta",
+	});
+
+	expect(answer.content).toEqual("Nova resposta");
+});
