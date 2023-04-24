f32fbbd2b8b36301515a5916d9fa36180475383c
andreenakashima <andreenakashima@gmail.com>
2023-04-24 10:55:26 -0300

    Slug value object

--- src/domain/use-cases/answer-quest.test.ts
+++ src/domain/use-cases/answer-quest.test.ts
@@ -1,9 +1,18 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
 
-test("create a answer", () => {
-	const answerQuestion = new AnswerQuestionUseCase();
-	const answer = answerQuestion.execute({
+const fakeAnswerRepository: AnswersRepository = {
+	create: async (answer: Answer) => {
+		return;
+	},
+};
+
+test("create a answer", async () => {
+	const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+	const answer = await answerQuestion.execute({
 		instructorId: "1",
 		questionId: "1",
 		content: "Nova resposta",
