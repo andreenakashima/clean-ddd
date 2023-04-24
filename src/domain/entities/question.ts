a9058480154b610a9746e77d0ba12ba466997120
andreenakashima <andreenakashima@gmail.com>
2023-04-24 11:25:28 -0300

    Base class entity

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from "node:crypto";
 import { Slug } from "./value-objects/slug";
+import { Entity } from "../../core/entities/entity";
 
 interface QuestionProps {
 	title: string;
@@ -7,18 +7,4 @@ interface QuestionProps {
 	slug: Slug;
 	authorId: string;
 }
-export class Question {
-	public id: string;
-	public title: string;
-	public content: string;
-	public slug: Slug;
-	public authorId: string;
-
-	constructor(props: QuestionProps, id?: string) {
-		this.id = id ?? randomUUID();
-		this.title = props.title;
-		this.slug = props.slug;
-		this.authorId = props.authorId;
-		this.content = props.content;
-	}
-}
+export class Question extends Entity<QuestionProps> {}
